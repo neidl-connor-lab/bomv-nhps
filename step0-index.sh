0e3f13740510d9b77de44c237d7941244b483950
Jacquelyn Turcinovic <jturcino@bu.edu>
2024-02-07 15:22:58 -0500

    updated indexing script

--- step0-index.sh
+++ step0-index.sh
@@ -1,20 +1,13 @@
 #!/bin/bash -l
 
 # qsub options
-#$ -P lasvchal
 #$ -l h_rt=48:00:00
 #$ -l mem_per_core=12G
 #$ -pe omp 16
 #$ -j y
 #$ -o log-$JOB_NAME.qlog
 
-# job info
-echo "=========================================================="
-echo "Start date : $(date)"
-echo "Job name : $JOB_NAME"
-echo "Job ID : $JOB_ID"
-echo "=========================================================="
-echo ""
+## setup -----------------------------------------------------------------------
 
 # functions
 mesg () { echo -e "[MSG] $@"; }
@@ -29,31 +22,27 @@ checkcmd () {
 }
 
 # default values and help message
-OVERHANG="100"
-OUTDIR="/restricted/projectnb/lasvchal/Jacquelyn/indices/star"
-FEAT="exon"
-ARGS="--runThreadN 16 --runMode genomeGenerate --limitGenomeGenerateRAM 200000000000"
-HELP="usage: qsub -N JOBNAME $(basename "$0") [OPTIONS] GTF FASTA 
+ARGS="--runThreadN 16 --runMode genomeGenerate --limitGenomeGenerateRAM 200000000000 --sjdbOverhang 100"
+HELP="usage: qsub -P PROJECT -N JOBNAME $(basename "$0") -o ODIR -x FEATURE -g GTF -f FASTA 
 
-positional argument:
-  GTF   combined GTF file
-  FASTA combined genome FASTA file
-
-optional arguments (default):
-  -l read length - 1 (${OVERHANG}) 
-  -o output directory (${OUTDIR})
-  -f feature for building transcripts (${FEAT})
+options:
+  -o output directory 
+  -x feature for building transcripts (exon | transcript)
+  -g GTF annotation file
+  -f reference genome FASTA file
   -h show this message and exit"
 
 # parsing arguments
-while getopts ":hl:o:f:" opt
+while getopts ":ho:x:g:f:" opt
 do
   case ${opt} in
-    l ) OVERHANG="${OPTARG}"
+    o ) ODIR="${OPTARG}"
       ;;
-    o ) OUTDIR="${OPTARG}"
+    x ) FEAT="${OPTARG}"
       ;;
-    f ) FEAT="${OPTARG}"
+    g ) GTF="${OPTARG}"
+      ;;
+    f ) FASTA="${OPTARG}"
       ;;
     h ) echo "${HELP}" && exit 0
       ;;
@@ -63,64 +52,91 @@ do
 done
 shift $((OPTIND -1))
 
-# get GTF and FASTA file(s)
-GTF="$1"
-FASTA="$2"
+## job info --------------------------------------------------------------------
 
-# check and add OUTDIR to ARGS
-if [ -z "${OUTDIR}" ]
-then
-  err "No output directory provided"
-elif [ -d "${OUTDIR}" ]
-then
-  mesg "Valid output directory: ${OUTDIR}"
-else
-  mesg "Creating output directory: ${OUTDIR}"
-  mkdir -p "${OUTDIR}"
-fi
-ARGS="${ARGS} --genomeDir '${OUTDIR}' --outFileNamePrefix '${OUTDIR}'"
-
-# check GTF
-if [ -z "${GTF}" ]
-then
-  err "GTF file not provided!"
-elif [ -f "${GTF}" ]
-then
-  mesg "Valid GTF file: ${GTF}"
-else
-  err "Invalid GTF file: ${GTF}"
-fi
-ARGS="${ARGS} --sjdbGTFfile '${GTF}'"
-
-# check genome
-if [ -z "${FASTA}" ]
-then
-  err "No genome FASTA file provided"
-elif [ -f "${FASTA}" ]
-then
-  mesg "Valid genome FASTA file: ${FASTA}"
-else
-  err "Invalid genome FASTA file: ${FASTA}"
-fi
-ARGS="${ARGS} --genomeFastaFiles '${FASTA}'"
-
-# check feature and overhang
-mesg "Using feature: ${FEAT}"
-ARGS="${ARGS} --sjdbGTFfeatureExon ${FEAT}"
-mesg "Using overhang: ${OVERHANG}"
-ARGS="${ARGS} --sjdbOverhang ${OVERHANG}"
-mesg "Done checking inputs"
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID"
+echo "=========================================================="
 echo ""
 
-# load STAR and show version
-mesg "Loading STAR"
+## check inputs ----------------------------------------------------------------
+mesg "STEP 0: CHECK INPUTS"
+
+# output directory
+if [ -z "$ODIR" ]
+then
+  err "No output directory provided"
+elif [ -d "$ODIR" ]
+then
+  mesg "Valid output directory: $ODIR"
+else
+  mesg "Creating output directory: $ODIR"
+  mkdir -p "$ODIR"
+fi
+ARGS="$ARGS --genomeDir '$ODIR' --outFileNamePrefix '$ODIR'"
+
+# check feature
+if [ -z "$FEAT" ]
+then
+  err "No feature provided"
+elif [ "$FEAT" == "exon" ]
+then
+  mesg "Using EXON as feature"
+elif [ "$FEAT" == "transcript" ]
+then
+  mesg "Using TRANSCRIPT as feature"
+else
+  err "Provided feature not recognized: $FEAT"
+fi
+ARGS="$ARGS --sjdbGTFfeatureExon ${FEAT}"
+
+# check GTF
+if [ -z "$GTF" ]
+then
+  err "GTF file not provided!"
+elif [ -f "$GTF" ]
+then
+  mesg "Valid GTF file: $GTF"
+else
+  err "Invalid GTF file: $GTF"
+fi
+ARGS="$ARGS --sjdbGTFfile '$GTF'"
+
+# check genome
+if [ -z "$FASTA" ]
+then
+  err "No genome FASTA file provided"
+elif [ -f "$FASTA" ]
+then
+  mesg "Valid genome FASTA file: $FASTA"
+else
+  err "Invalid genome FASTA file: $FASTA"
+fi
+ARGS="$ARGS --genomeFastaFiles '$FASTA'"
+
+# done checking inputs
+mesg "Done checking inputs!"
+echo ""
+
+## run indexing command --------------------------------------------------------
+mesg "STEP 1: BUILD INDEX"
+
+# load STAR
 module load star/2.7.1a
-echo "STAR $(STAR --version)"
 
 # run STAR index
 mesg "Running STAR index"
-CMD="STAR ${ARGS}"
-mesg "CMD: ${CMD}"
-eval "${CMD}"
+CMD="STAR $ARGS"
+mesg "CMD: $CMD"
+eval "$CMD"
 checkcmd "STAR index"
+echo ""
 
+## Done! output environment ----------------------------------------------------
+module list
+echo "STAR $(STAR --version)"
+
+mesg "PIPELINE COMPLETE"
+echo ""
